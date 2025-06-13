4c5246d4f0dcce05c79836b3b2b007aaad815f04
Mc Peterson C. Mercader <mercadermc4gmail.com@Mcs-MacBook-Air.local>
2025-06-14 00:58:03 +0800

    Save local changes before pulling

--- src/app/pages/classes/classPages/practicum2/practicum2.component.ts
+++ src/app/pages/classes/classPages/practicum2/practicum2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +8,17 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-practicum2',
   imports: [CommonModule, MatInputModule, MatIconModule, MatButtonModule],
+=======
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard, MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-practicum2',
+  imports: [CommonModule, MatInputModule, MatIconModule, MatButtonModule, MatCard, MatCardModule, NgIf, NgFor],
+>>>>>>> 38614d6 (Save local changes before pulling)
   templateUrl: './practicum2.component.html',
   styleUrl: './practicum2.component.css',
 })
@@ -20,4 +32,41 @@ export class Practicum2Component {
   isActiveTab(tabName: string): boolean {
     return this.activeTab === tabName;
   }
+<<<<<<< HEAD
+=======
+
+  selectedActivity: any = null;
+  enterActivity(activity: any) {
+    this.selectedActivity = activity;
+    this.setActiveTab("Activities/Resources");
+  }
+
+  activies = [
+    {
+      activityName: "Research and Development",
+      activityDate: "Saturday, April 4, 2025, 00:00 PM",
+      resources: [
+        {
+          resourceDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+          resourceName: "Resource 1",
+          resourceLink: "https://material.angular.dev/",
+          resourceType: "Video",
+        },
+      ],
+    },
+    {
+      activityName: "activity 2: BMC",
+      activityDate: "Monday, 04:00 pm - 05:00 pm",
+    },
+    {
+      activityName: "Activity 3: Thesis Proposal",
+      activityDate: "Tuesday, 08:00 AM - 10:00 AM",
+    },
+    {
+      activityName: "Activity 4: 100 paragraph essay",
+      activityDate: "Monday, 12:00 PM - 02:30 PM",
+    },
+  ];
+>>>>>>> 38614d6 (Save local changes before pulling)
 }
--- src/app/pages/classes/classPages/thesis-writing2/thesis-writing2.component.ts
+++ src/app/pages/classes/classPages/thesis-writing2/thesis-writing2.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCard, MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'app-thesis-writing2',
-  imports: [CommonModule, MatInputModule, MatIconModule, MatButtonModule],
-  templateUrl: './thesis-writing2.component.html',
-  styleUrl: './thesis-writing2.component.css',
+  selector: "app-thesis-writing2",
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCard,
+    MatCardModule,
+    MatIconModule,
+    MatIcon,
+    NgFor,
+  ],
+  templateUrl: "./thesis-writing2.component.html",
+  styleUrls: ["./thesis-writing2.component.css"],
 })
 export class ThesisWriting2Component {
-  activeTab: string = 'Class Feed';
+  activeTab: string = "Class Feed";
+
 
   setActiveTab(tabName: string): void {
     this.activeTab = tabName;
@@ -20,4 +32,38 @@ export class ThesisWriting2Component {
   isActiveTab(tabName: string): boolean {
     return this.activeTab === tabName;
   }
+
+  selectedActivity: any = null;
+  enterActivity(activity: any) {
+    this.selectedActivity = activity;
+    this.setActiveTab("Activities/Resources");
+  }
+
+  activies = [
+    {
+      activityName: "Research and Development",
+      activityDate: "Saturday, April 4, 2025, 00:00 PM",
+      resources: [
+        {
+          resourceDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+          resourceName: "Resource 1",
+          resourceLink: "https://material.angular.dev/",
+          resourceType: "Video",
+        },
+      ],
+    },
+    {
+      activityName: "activity 2: BMC",
+      activityDate: "Monday, 04:00 pm - 05:00 pm",
+    },
+    {
+      activityName: "Activity 3: Thesis Proposal",
+      activityDate: "Tuesday, 08:00 AM - 10:00 AM",
+    },
+    {
+      activityName: "Activity 4: 100 paragraph essay",
+      activityDate: "Monday, 12:00 PM - 02:30 PM",
+    },
+  ];
 }
